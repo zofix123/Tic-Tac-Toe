2ce84de01a6d6b7a505999b2b805ea590586996d
zofix123 <tvinkcs56@gmail.com>
2024-10-19 14:21:28 +0300

    rules

--- main.py
+++ main.py
@@ -27,17 +27,29 @@ def draw_grid():
         pygame.draw.line(screen, grid, (0, i*(screen_height//3)), (screen_width, i*(screen_height//3)), line_width)
         pygame.draw.line(screen, grid, (i*(screen_width//3), 0), (i*(screen_width//3), screen_height), line_width)
 
+def draw_markers():
+    x_pos = 0
+    for x in game_state:
+        y_pos = 0
+        for y in x:
+            if y == 1:
+                pygame.draw.line(screen, (0, 255, 0), (x_pos*screen_width//3 + 15, y_pos*screen_height//3 + 15), (x_pos*screen_width//3 + 85, y_pos*screen_height//3 + 85), line_width)
+                pygame.draw.line(screen, (0, 255, 0), (x_pos*screen_width//3 + 15, y_pos*screen_height//3 + 85), (x_pos*screen_width//3 + 85, y_pos*screen_height//3 + 15), line_width)
+            if y == -1:
+                pygame.draw.circle(screen, (255, 0, 0), (x_pos*screen_width//3 + 50, y_pos*screen_height//3 + 50), 38, line_width)
+            y_pos += 1
+        x_pos += 1
+
 run = True
 while run:
 
     draw_grid()
-
+    draw_markers()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(pos)
             mouse_x = pos[0]
             mouse_y = pos[1]
             if game_state[mouse_x // (screen_width // 3)][mouse_y // (screen_height // 3)] == 0:
